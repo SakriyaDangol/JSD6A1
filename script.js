5e341551cea56c67026010617db552f487487ce5
SakriyaDangol <sakriyadangol003@gmail.com>
2021-06-09 16:36:09 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var number = 120; // intiger
+var stringOne = '5150'; // string
+var stringTwo = 'Two Hundred Thirty'; //string
+
+// printing one by one
+console.log(number);
+console.log(stringOne);
+console.log(stringTwo);
+
+// printing in same line
+console.log(number, stringOne, stringTwo);
+
+// printing in same statement but different line
+console.log(number + '\n' + stringOne + '\n' + stringTwo);
+
+// converting string to number
+stringOne = 5150;
+
+console.log(stringOne);
